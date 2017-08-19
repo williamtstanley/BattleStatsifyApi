0ad0c2b16cba78b81036117e8b15dd5266261e7d
Travis Stanley <travisstanley@Admins-MacBook-Air.local>
2017-08-18 22:22:29 -0700

    forgot to rename require after making changes to the structure

--- entryService/entryService.js
+++ entryService/entryService.js
@@ -1,6 +1,6 @@
 const app = require('express')();
-const Routes = require('./routing/routes');
-const routes = new Routes(app);
+const Router = require('./routing/router');
+const routes = new Router(app);
 
 app.listen(3000, function() {
 	console.log('EntryService started listening on:', this.address().port)
