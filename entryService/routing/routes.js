9d4f6306a13ef35d53720650e0ddc24be2b4ebb6
Travis Stanley <travisstanley@Admins-MacBook-Air.local>
2017-08-20 17:14:41 -0700

    add api/ end point in entryServices hmm I should rename that at some point.

--- entryService/routing/routes.js
+++ entryService/routing/routes.js
@@ -2,4 +2,9 @@ module.exports = {
 	'/': {
 		'get': (req, res) => res.send('Hello World!'),
 	},
+  '/api/:summonerName': {
+    'get': (req, res) => {
+      res.json(req.params)
+    }
+  }
 };
---
+++ server.config.js
@@ -0,0 +1,18 @@
+const ENV = process.env.NODE_ENV;
+const path = process.env.NODE_PATH;
+const list = ['entryService'];
+
+module.exports = {
+	apps: list.map((server) => ({
+		name: server,
+		script: `./${server}/${server}.js`,
+		log_date_format: 'YYYY-MM-DD HH:mm',
+		watch: (ENV === 'development') ? './' : false,
+		ignore_watch: ['node_modules', '.git'],
+		node_args: (ENV === 'development') ? '--inspect' : '',
+		env: {
+			'NODE_ENV': ENV,
+			'NODE_PATH': path,
+		},
+	})),
+};
